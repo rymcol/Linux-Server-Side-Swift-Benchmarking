44fc38a324549a97cd04ed9adfa2fa7a5a4de75d
chris of the clan <chris@clanofthecloud.com>
2016-10-09 12:26:49 +0100

    addind fields to an object is notoriously "slow" compared to fixed fields

--- NodeJSON/app/CommonHandler.js
+++ NodeJSON/app/CommonHandler.js
@@ -3,12 +3,28 @@ module.exports = {
     makeJSON: function () {
 	    var dict = {}
 
-	    for(i = 1; i < 11; i++) {
+	    for(var i = 1; i < 11; i++) {
 		    var randomNumber = Math.floor(Math.random() * 1000);
 		    var testNum = "Test Number " + i;
 		    dict[testNum] = randomNumber;
 	    }
 
 	    return dict
+    },
+
+		// avoid adding fields to an object... gains 500req/s
+    makeJSONfast: function () {
+			return {
+				"Test Number 1": Math.floor(Math.random() * 1000),
+				"Test Number 2": Math.floor(Math.random() * 1000),
+				"Test Number 3": Math.floor(Math.random() * 1000),
+				"Test Number 4": Math.floor(Math.random() * 1000),
+				"Test Number 5": Math.floor(Math.random() * 1000),
+				"Test Number 6": Math.floor(Math.random() * 1000),
+				"Test Number 7": Math.floor(Math.random() * 1000),
+				"Test Number 8": Math.floor(Math.random() * 1000),
+				"Test Number 9": Math.floor(Math.random() * 1000),
+				"Test Number 10": Math.floor(Math.random() * 1000),
+			}
     }
 };
